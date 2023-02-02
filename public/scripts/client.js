5e1068389816ee530272e69de893c2dd611013df
sudhasarathi <sudhav158@email.com>
2023-02-01 17:06:30 -0700

    deleted and refacted data on client side

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -40,34 +40,22 @@ const renderTweets = (data) => {
     $('#tweets-container').append(newTweet);
   }
 };
-
-
-const data = [
-  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-   "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+const loadTweets = () => {
+  $.get('/tweets').then((data) => {
+    renderTweets(data);
+  });
+};
 
 $( document ).ready(function() {
 
-  renderTweets(data);
+  
+
+  $('.new-tweet form').submit(function(event) {
+    event.preventDefault();
+    const tweetData = $(this).serialize();
+    $.post('/tweets/', tweetData).then(() => {
+      loadTweets();
+    });
+  });
 
 });
